88289652011b0853a52113a5ae46d88aa52f2c33
oddnose <teo.ruokolainen@outlook.com>
2026-06-09 17:30:06 +0200

    Add simulation option to cli

--- src/8086emu.cpp
+++ src/8086emu.cpp
@@ -1,5 +1,6 @@
 #include "8086_text.h"
 #include "decoder.h"
+#include "simulator.hpp"
 #include <cstring>
 #include <iostream>
 
@@ -10,19 +11,22 @@ int main(int arg_count, char **args)
 		std::cerr << "Usage: " << args[0] << " <8086 binary file>" << std::endl;
 		std::cerr << "Flags:" << std::endl;
 		std::cerr << "-d: print debug info" << std::endl;
+		std::cerr << "-s: run simulation and print out memory state after program" << std::endl;
 		std::exit(1);
 	}
 
 	std::vector<Instruction> instructions;
 	bool debug = false;
+	bool run_simulate = false;
 
 	for (int arg_index = 1; arg_index < arg_count; arg_index++) {
 		char* arg = args[arg_index];
 
 		if (strcmp(arg, "-d") == 0) {
 			debug = true;
-		}
-		else {
+		} else if (strcmp(arg, "-s") == 0) {
+			run_simulate = true;
+		} else {
 			instructions = decode_asm_file(arg);
 		}
 	}
@@ -37,8 +41,13 @@ int main(int arg_count, char **args)
 			inst.print_debug();
 			std::cout << "\n";
 		}
-		std::exit(0);
+	} else {
+		std::cout << get_print_instructions(instructions) << std::endl;
 	}
 
-	std::cout << get_print_instructions(instructions) << std::endl;
+	if (run_simulate) {
+		std::cout << "Simulation:" << std::endl;
+		simulate(instructions);
+	}
+
 }
